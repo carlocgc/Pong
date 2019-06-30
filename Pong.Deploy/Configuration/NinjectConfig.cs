cdac014e50b6141ff0c3419004f7a9034a7cad37
CarloCGC <carlocgc@hotmail.com>
2019-06-30 12:25:46 +0100

    Removed Ninject

--- Pong.Deploy/Configuration/NinjectConfig.cs
+++ Pong.Deploy/Configuration/NinjectConfig.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pong.Deploy
+namespace Pong.Deploy.Configuration
 {
-    public class Class1
+    public class NinjectConfig
     {
     }
 }
