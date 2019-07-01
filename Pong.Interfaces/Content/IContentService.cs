09b74409999b624cf5e7c017bfaaee2a03c47f0d
CarloCGC <carlocgc@hotmail.com>
2019-07-01 20:02:01 +0100

    Added content manager wrapper

---
+++ Pong.Interfaces/Content/IContentService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong.Interfaces.Content
+{
+    /// <summary>
+    /// Wrapper for monogame content
+    /// </summary>
+    public interface IContentService
+    {
+        /// <summary> Load the content of the given type </summary>
+        /// <typeparam name="T"></typeparam>        
+        /// <param name="assetName"></param>
+        /// <returns></returns>
+        T Load<T>(String assetName);
+
+        T LoadLocalized<T>(String assetName);
+
+        /// <summary>
+        /// Unloads all loaded content
+        /// </summary>
+        void Unload();
+    }
+}
